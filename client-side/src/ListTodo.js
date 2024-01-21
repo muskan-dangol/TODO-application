2116a066f31850a323681b9ef60168ae9e1fd9cb
muskan-dangol <dangolmuskan19@gmail.com>
2024-01-21 15:52:42 +0200

    replaced table and input with mui table and mui InputField

--- client-side/src/ListTodo.js
+++ client-side/src/ListTodo.js
@@ -1,5 +1,12 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import EditTodo from "./components/EditTodo";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const ListTodos = () => {
   const [todos, setTodos] = useState([]);
@@ -7,7 +14,7 @@ const ListTodos = () => {
   //delete function
   const deleteTodo = async (id) => {
     try {
-      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+      await fetch(`http://localhost:5000/todos/${id}`, {
         method: "DELETE",
       });
 
@@ -29,36 +36,39 @@ const ListTodos = () => {
   useEffect(() => {
     getTodos();
   }, []);
+  
   return (
-    <Fragment>
-      <table className="table mt-5 text-center">
-        <thead>
-          <tr>
-            <th>Description</th>
-            <th>Edit</th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          {todos.map((todo) => (
-            <tr key={todo.todo_id}>
-              <td>{todo.description}</td>
-              <td>
-                <EditTodo todo={todo} />
-              </td>
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => deleteTodo(todo.todo_id)}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </Fragment>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <TableContainer component={Paper} sx={{ maxWidth: 550, mt: 5 }}>
+        <Table sx={{ maxWidth: 550 }} aria-label="simple table">
+          <TableHead sx={{backgroundColor: "lightgray"}}>
+            <TableRow>
+              <TableCell>Description</TableCell>
+              <TableCell align="right">Edit</TableCell>
+              <TableCell align="right">Delete</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {todos.map((todo) => (
+              <TableRow key={todo.todo_id}>
+                <TableCell align="left">{todo.description}</TableCell>
+                <TableCell align="right">
+                  <EditTodo todo={todo} />
+                </TableCell>
+                <TableCell align="right">
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => deleteTodo(todo.todo_id)}
+                  >
+                    Delete
+                  </button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 };
 
